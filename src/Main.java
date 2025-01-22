9b28764bd8f77bdd1a620397d0b4a3fb91ea1be9
Maximus301191 <maxalut301191@gmail.com>
2025-01-22 16:57:52 +0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-      int amount = 20_000; //стоимость билета
+        int amount = 20_000; //стоимость билета
         int bonus = 20; //стоимость 1 бонуса
 
         int mile = amount / bonus; //итоговая сумма бонусов
@@ -9,4 +9,4 @@ public class Main {
     }
 
 
-    }
+}
